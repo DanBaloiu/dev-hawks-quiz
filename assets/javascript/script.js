283733ffe3976530852297fe2423257388d7e840
Trillspectre <andrewbullimore@gmail.com>
2025-08-19 18:21:10 +0100

    added fall back if api fails and added html encoding throughout the code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -23,47 +23,67 @@ function decodeHTMLEntities(text) {
     return txt.value;
 }
 
-fetch("https://opentdb.com/api.php?amount=10&type=multiple")
-.then((res) => {
-    return res.json();
-})
-.then(loadedQuestions => {
 
-    questions = loadedQuestions.results.map(loadedQuestion => {
-        const formattedQuestion = {
-            question: decodeHTMLEntities(loadedQuestion.question),
-        };
-        const answerChoices = [...loadedQuestion.incorrect_answers];
-        formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-        answerChoices.splice(
-            formattedQuestion.answer - 1,
-            0,
-            loadedQuestion.correct_answer
-        );
-
-        answerChoices.forEach((choice, index) => {
-            formattedQuestion["choice" + (index + 1)] = decodeHTMLEntities(choice);
+// Load questions from API or local file
+function loadQuestions(callback) {
+    fetch("https://opentdb.com/api.php?amount=10&type=multiple")
+        .then((res) => res.json())
+        .then(loadedQuestions => {
+            questions = loadedQuestions.results.map(loadedQuestion => {
+                const formattedQuestion = {
+                    question: decodeHTMLEntities(loadedQuestion.question),
+                };
+                const answerChoices = [...loadedQuestion.incorrect_answers];
+                formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+                answerChoices.splice(
+                    formattedQuestion.answer - 1,
+                    0,
+                    loadedQuestion.correct_answer
+                );
+                answerChoices.forEach((choice, index) => {
+                    formattedQuestion["choice" + (index + 1)] = decodeHTMLEntities(choice);
+                });
+                return formattedQuestion;
+            });
+            callback();
+        })
+        .catch(err => {
+            // If API fails, load from local questions.json
+            fetch("assets/javascript/questions.json")
+                .then(res => res.json())
+                .then(localQuestions => {
+                    questions = localQuestions.map(q => {
+                        const formattedQuestion = {
+                            question: decodeHTMLEntities(q.question),
+                            answer: q.answer
+                        };
+                        // Decode all choices
+                        for (let i = 1; i <= 4; i++) {
+                            formattedQuestion["choice" + i] = decodeHTMLEntities(q["choice" + i]);
+                        }
+                        return formattedQuestion;
+                    });
+                    callback();
+                })
+                .catch(localErr => {
+                    console.error("Failed to load questions from both API and local file.", localErr);
+                });
         });
-        return formattedQuestion;
-    });
-
-    startGame();
-
-})
-.catch(err => {
-    console.error(err);
-});
+}
 //constants
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 5;
+
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-   
     getNewQuestion();
 };
+
+// Load questions and start the game
+loadQuestions(startGame);
 
 
 getNewQuestion = () => {
@@ -74,11 +94,12 @@ getNewQuestion = () => {
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+    // Always decode before display (extra safety)
+    question.innerText = decodeHTMLEntities(currentQuestion.question);
 
     choices.forEach(choice => {
         const number = choice.dataset["number"];
-        choice.innerText = currentQuestion["choice" + number];
+        choice.innerText = decodeHTMLEntities(currentQuestion["choice" + number]);
     });
     availableQuestions.splice(questionIndex, 1);
 
