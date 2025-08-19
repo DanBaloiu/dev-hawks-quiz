21bd484f50bf3555eb7a57cd53e10db5afc239b9
kens-i <kensi.lee@outlook.com>
2025-08-19 17:06:01 +0100

    style highscores using css

--- assets/javascript/highscore.js
+++
@@ -1,8 +0,0 @@
-const highScoreList = document.getElementById("highScoreList");
-const highScore = JSON.parse(localStorage.getItem("highScore")) || [];
-
-highScoreList.innerHTML = highScore
-  .map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-  })
-  .join("");
